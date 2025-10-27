5250512a728edf9474c8a707341897990bab267a
blanchemarion <101418560+blanchemarion@users.noreply.github.com>
2025-10-27 18:55:17 +0100

    test

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   // GitHub Pages project page configuration
-  base: '/bloom-lab/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
